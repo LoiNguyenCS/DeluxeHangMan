8982d19538fca7a2f32ab1adc7d3825074a7772d
unknown <ln3@njit.edu>
2023-04-11 23:41:58 -0400

    Change my mind, app.ts should be the one to start a server

--- src/HangManController.ts
+++ src/HangManController.ts
@@ -1,34 +1,16 @@
 /**
  * This is the place where we control the whole HangMan server in general
  */
-import express from 'express';
 import { Team } from './Team';
 import { Player } from './Player';
-import path from 'path';
 import { Game } from './Game';
 
 export class HangManController {
-  private port: number;
   private listOfPlayers: Player[];
   private listOfTeams: Team[];
   private listOfGameSession: Game[];
-  private serverStarted = false;
   constructor() {
-    this.port = 2010;
-  }
-  startServer() {
-    if (this.serverStarted === true) {
-        throw new Error('Server is started already!');
-    }
-    const app = express();
-    const port = this.port;
-    app.use('/dist', express.static('dist'));
-    app.get('/', (_, res) => {
-        res.sendFile(path.join(__dirname, './index.html'));
-      });
-    app.listen(port, () => {
-       return console.log(`Express is listening at http://localhost:${port}`);    
-    });
+    console.log("New HangManController created!");
   }
   addPlayer(newPlayer: Player) {
     this.listOfPlayers.push(newPlayer);
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,17 @@
  * This is the place where we receive connection from Covey.Town and perform corresponding tasks
  */
 import { HangManController } from "./HangManController";
+import express from 'express';
+import path from 'path';
 
+const app = express();
+const port = 2010;
+app.use('/dist', express.static('dist'));
+app.get('/', (_, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+  });
+app.listen(port, () => {
+   return console.log(`Express is listening at http://localhost:${port}`);    
+});
 const thisController = new HangManController();
-thisController.startServer();
+
