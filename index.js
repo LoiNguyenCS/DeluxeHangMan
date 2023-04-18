0bd83a58e3fe94eb6e60ddb68e81df119d46be68
unknown <ln3@njit.edu>
2023-04-18 12:21:19 -0400

    add dynamic routing

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/welcome.html');
 });
 
 io.sockets.on('connection', function (socket) {
   console.log(`a user connected with id ${socket.id}`);
-  const roomId = `room-${socket.id}`;
+  const roomId = `${socket.id}`;
+  console.log(`server created /${roomId}`);
+  // Redirect the player to their unique URL
+  app.get(`/${roomId}`, (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+  });
+  app.get('/join', (req, res) => {
+    res.redirect(`/${roomId}`);
+  });
+  
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
@@ -21,7 +30,6 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-    
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
