f01201115148ba627e72c171d8f1ddb001550eed
Gman <gmottn718@gmail.com>
2023-05-04 11:50:09 -0400

    Bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const GameSession = require('./component/GameSession')
-const fs = require('fs');
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const http = require('http')
@@ -18,12 +18,11 @@ app.get('/', (req, res) => {
 
 /**
  * If a player joins as an individual, a new webpage will be served at localhost:3000/socket_id. Otherwise, he will join
- * another webpage created by some previous users. For convenience, we don't discriminate between individual mode and 
+ * another webpage created by some previous users. For convenience, we don't discriminate between individual mode and
  * team mode at the backend level. Every game session is regarded as a GameSession instance equally. Right now we are
  * having a lot of console logs printed because it is convenient for debugging. We can delete them in the future.
  */
 io.sockets.on('connection', function (socket) {
-  
   //Individual creates a game
   socket.on('joinAsIndividual', () => {
     const roomId = `${socket.id}`
@@ -32,46 +31,51 @@ io.sockets.on('connection', function (socket) {
       res.sendFile(__dirname + '/public/index.html')
     })
     currentSession = new GameSession()
-    //currentSession.addPlayer(roomId)
+
     sessionList[roomId] = currentSession
+    setTimeout(timerFunction, 1000)
   })
- 
+  //determines if the game ID given exists or not, if it does then lets know that the game can be joined.
+  socket.on('canJoin', (teamId) => {
+    if (teamId in sessionList) {
+      socket.emit('joinGame', teamId)
+    } else {
+      socket.emit('declineJoin')
+    }
+  })
+
   //New player added to game, initialize them and make sure they are prepared and updated
   socket.on('addPlayer', (name) => {
-    name = CheckNameUnique(name,socket);
-    currentSession.addPlayer(name,socket.id);
-    UpdateOneWord(socket);
-    UpdateOneHang(socket);
-    UpdateOneHeader(socket);
-    UpdateKeyboard(socket);
-
+    name = CheckNameUnique(name, socket)
+    currentSession.addPlayer(name, socket.id)
+    UpdateOneWord(socket)
+    UpdateOneHang(socket)
+    UpdateOneHeader(socket)
+    UpdateKeyboard(socket)
   })
   //counts down the timer for everyone, conforming to certain events depending on game state when counter reaches zero
-  setInterval(function(){
-    if(currentSession.Seconds != 0 && currentSession.status != 'True_Wait'){
-      currentSession.Seconds -= 1;
-      UpdateTimer();
-      
-    }else{
-      switch(currentSession.status){
+  function timerFunction() {
+    if (currentSession.Seconds != 0 && currentSession.status != 'True_Wait') {
+      currentSession.Seconds -= 1
+      UpdateTimer()
+    } else {
+      switch (currentSession.status) {
         case 'Wait':
-          currentSession.status =  'In Progress';
-          currentSession.PickWord();
-          UpdateWord();
+          currentSession.status = 'In Progress'
+          currentSession.PickWord()
+          UpdateWord()
         case 'In Progress':
-          currentSession.SwitchPlayer();
-          UpdateHeader();
-          break;
-        case "Win":
-        case "Lose":
-          
-          currentSession.RestartGame();
+          currentSession.SwitchPlayer()
+          UpdateHeader()
+          break
+        case 'Win':
+        case 'Lose':
+          currentSession.RestartGame()
           ResetPlayers()
-          
       }
     }
-    
-  }, 2000);
+    setTimeout(timerFunction, 1000)
+  }
 
   /*
    When a user clicks a button, the client side will send a keyPressed message to the server side. 
@@ -81,27 +85,27 @@ io.sockets.on('connection', function (socket) {
    and user 1 will send a message to the server. The server will then send a message to all the users, specifying that
    letter "E" has been pressed for team A. Then other members in team A will receive the message and have their webpages updated
    */
-  socket.on('KeyPressed', (letter,name) => {
-    if(currentSession.ActivePlayer == name && currentSession.status == "In Progress"){
+  socket.on('KeyPressed', (letter, name) => {
+    if (
+      currentSession.ActivePlayer == name &&
+      currentSession.status == 'In Progress'
+    ) {
       currentSession.SwitchPlayer()
-      UpdateHeader();
-      HideKey(letter);
-    if(currentSession.UpdateWord(letter)){
-      UpdateWord();
-    }else{
-      UpdateHang();
+      UpdateHeader()
+      HideKey(letter)
+      if (currentSession.UpdateWord(letter)) {
+        UpdateWord()
+      } else {
+        UpdateHang()
+      }
+      checkIfGameOver()
     }
-    checkIfGameOver();
-    
-  }
-
-    
   })
-  
+
   //Remove the player when player disconnects
   socket.on('disconnect', () => {
     currentSession.RemovePlayer(socket.id)
-    UpdateHeader();
+    UpdateHeader()
   })
 })
 
@@ -111,84 +115,81 @@ server.listen(3000, () => {
 })
 
 //Checks to see if the game has either been won or lost
-function checkIfGameOver(){
-  var state = currentSession.status;
-  if(state == "Win" || state == "Lose"){
-    UpdateHeader();
-  }else{
+function checkIfGameOver() {
+  var state = currentSession.status
+  if (state == 'Win' || state == 'Lose') {
+    UpdateHeader()
+  } else {
     return
   }
 }
 
 //Hides given key for all players
-function HideKey(letter){
-  io.emit('HideKey',letter);
+function HideKey(letter) {
+  io.emit('HideKey', letter)
 }
 //updates header for all players
-function UpdateHeader(){
-  io.emit('UpdateHeader',currentSession.header);
+function UpdateHeader() {
+  io.emit('UpdateHeader', currentSession.header)
 }
 // updates how the current word is displayed for all players
-function UpdateWord(){
-  io.emit('UpdateWord',currentSession.word);
+function UpdateWord() {
+  io.emit('UpdateWord', currentSession.word)
 }
 //Updates Timer for all players
-function UpdateTimer(){
-  io.emit('UpdateTimer',currentSession.Seconds);
+function UpdateTimer() {
+  io.emit('UpdateTimer', currentSession.Seconds)
 }
 //Updates hangman image for all players
-function UpdateHang(){
-  let image = fs.readFileSync(currentSession.hangImage);
-  io.emit('UpdateHang',image)
+function UpdateHang() {
+  let image = fs.readFileSync(currentSession.hangImage)
+  io.emit('UpdateHang', image)
 }
 //causes all players to reset their keyboard keys
-function ResetKeyboard(){
-  io.emit('ResetKeyboard');
+function ResetKeyboard() {
+  io.emit('ResetKeyboard')
 }
 //Removes the word for all players
-function RemoveWord(){
+function RemoveWord() {
   io.emit('RemoveWord')
 }
 //Updates the keyboard of the user who just joined
-function UpdateKeyboard(socket){
-  
-  socket.emit('UpdateKeyboard',currentSession.guessedLetters);
+function UpdateKeyboard(socket) {
+  socket.emit('UpdateKeyboard', currentSession.guessedLetters)
 }
 
 //ALL BELOW function as their none "One" counterparts, only difference is that these act on the player who just joined the game and needs to be updated
-function UpdateOneHeader(socket){
-  socket.emit('UpdateHeader',currentSession.header);
+function UpdateOneHeader(socket) {
+  socket.emit('UpdateHeader', currentSession.header)
 }
-function UpdateOneWord(socket){
-  socket.emit('UpdateWord',currentSession.word);
+function UpdateOneWord(socket) {
+  socket.emit('UpdateWord', currentSession.word)
 }
-function UpdateOneTimer(socket){
-  socket.emit('UpdateTimer',currentSession.Seconds);
+function UpdateOneTimer(socket) {
+  socket.emit('UpdateTimer', currentSession.Seconds)
 }
-function UpdateOneHang(socket){
-  let image = fs.readFileSync(currentSession.hangImage);
-  socket.emit('UpdateHang',image)
+function UpdateOneHang(socket) {
+  let image = fs.readFileSync(currentSession.hangImage)
+  socket.emit('UpdateHang', image)
 }
 
 //Checks if the given username is unique, if not then modifies it and sends it back so it is unique
-function CheckNameUnique(name,socket){
-  let num = currentSession.CheckNameUnique(name);
-  if(num){
-    name = name +"(" + num.toString() + ")";
-    socket.emit('UpdateName',name);
-    console.log("FIXED NAME" + name);
+function CheckNameUnique(name, socket) {
+  let num = currentSession.CheckNameUnique(name)
+  if (num) {
+    name = name + '(' + num.toString() + ')'
+    socket.emit('UpdateName', name)
+    console.log('FIXED NAME' + name)
   }
-  console.log("created " + num.toString());
+  console.log('created ' + num.toString())
   return name
 }
 
 //Resets all players for a ew game
-function ResetPlayers(){
-  ResetKeyboard();
-  UpdateHeader();
-  UpdateWord();
-  UpdateHang();
-  RemoveWord();
-
+function ResetPlayers() {
+  ResetKeyboard()
+  UpdateHeader()
+  UpdateWord()
+  UpdateHang()
+  RemoveWord()
 }
-
--- test/Queue.test.js
+++ test/Queue.test.js
@@ -8,102 +8,113 @@ describe('GameSession', () => {
   })
   describe('enqueue', () => {
     Q = new Queue()
-
+   
     it('adds item to Queue', () => {
-      Q.enqueue('Test')
-      expect(Q.items[0]).toBe('Test')
-    })
-  })
-  describe('dequeue', () => {
+        Q.enqueue("Test")
+        expect(Q.items[0]).toBe("Test");
+    });
+    
+});
+describe('dequeue', () => {
     Q = new Queue()
-
+   
     it('Removes front most item', () => {
-      Q.enqueue('Test')
-      Q.enqueue('Test2')
-      Q.dequeue()
-      expect(Q.items[0]).toBe('Test2')
-    })
+        Q.enqueue("Test")
+        Q.enqueue("Test2")
+        Q.dequeue();
+        expect(Q.items[0]).toBe("Test2");
+    });
     it('Returns front most item', () => {
-      Q.enqueue('Test')
-      Q.enqueue('Test2')
-
-      expect(Q.dequeue()).toBe('Test')
-    })
-  })
-  describe('peek', () => {
+        Q.enqueue("Test")
+        Q.enqueue("Test2")
+        
+        expect(Q.dequeue()).toBe("Test");
+    });
+    
+});
+describe('peek', () => {
     Q = new Queue()
-
+   
     it('returns first item without removing it', () => {
-      Q.enqueue('Test')
-      Q.enqueue('Test2')
-      expect(Q.peek()).toBe('Test')
-      expect(Q.items[0]).toBe('Test')
-    })
-  })
-  describe('size', () => {
+        Q.enqueue("Test")
+        Q.enqueue("Test2")
+        expect(Q.peek()).toBe("Test");
+        expect(Q.items[0]).toBe("Test");
+    });
+    
+});
+describe('size', () => {
     Q = new Queue()
-
+   
     it('returns size of the queue', () => {
-      expect(Q.size()).toBe(0)
-      Q.enqueue('Test')
-      expect(Q.size()).toBe(1)
-      Q.enqueue('Test2')
-      expect(Q.size()).toBe(2)
-      Q.dequeue()
-      expect(Q.size()).toBe(1)
-    })
-  })
-  describe('isEmpty', () => {
+        expect(Q.size()).toBe(0)
+        Q.enqueue("Test")
+        expect(Q.size()).toBe(1)
+        Q.enqueue("Test2")
+        expect(Q.size()).toBe(2)
+        Q.dequeue()
+        expect(Q.size()).toBe(1)
+        
+    });
+    
+});
+describe('isEmpty', () => {
     Q = new Queue()
-
+   
     it('returns true if queue is empty', () => {
-      expect(Q.isEmpty()).toBe(true)
-    })
+        expect(Q.isEmpty()).toBe(true);
+        
+    });
     it('returns false if queue is not empty', () => {
-      Q.enqueue('Test')
-      expect(Q.isEmpty()).toBe(false)
-    })
-  })
-  describe('forceOut', () => {
+        Q.enqueue("Test")
+        expect(Q.isEmpty()).toBe(false);
+        
+    });
+    
+});
+describe('forceOut', () => {
     Q = new Queue()
-
+   
     it('Removes the given item no matter where it is', () => {
-      Q.enqueue('Test1')
-      Q.enqueue('Test2')
-      Q.enqueue('Test3')
-      console.log(Q.items)
-      Q.forceOut('Test2')
-      console.log(Q.items)
-      Q.dequeue()
-      console.log(Q.items)
-      expect(Q.peek()).toBe('Test3')
-    })
-  })
-  describe('CountOccurrence', () => {
+        Q.enqueue("Test1")
+        Q.enqueue("Test2")
+        Q.enqueue("Test3")
+        Q.forceOut("Test2");
+        Q.dequeue()
+        expect(Q.peek()).toBe("Test3");
+    });
+   
+
+    
+});
+describe('CountOccurrence', () => {
     Q = new Queue()
-
+   
     it('Counts # of occurence of a particular item in the queue', () => {
-      Q.enqueue('Test')
-      Q.enqueue('Test')
-      Q.enqueue('Test2')
-
-      expect(Q.CountOccurrence('Test')).toBe(2)
-    })
+        Q.enqueue("Test")
+        Q.enqueue("Test")
+        Q.enqueue("Test2")
+        
+        expect(Q.CountOccurrence("Test")).toBe(2);
+    });
     it('Counts # of occurence of a particular item in the queue even if it has something in paranthesis in the end', () => {
-      Q.enqueue('Test')
-      Q.enqueue('Test(1)')
-      Q.enqueue('Test2')
-      Q.enqueue('Test2(1)')
-
-      expect(Q.CountOccurrence('Test')).toBe(2)
-    })
+        Q.enqueue("Test")
+        Q.enqueue("Test(1)")
+        Q.enqueue("Test2")
+        Q.enqueue("Test2(1)")
+        
+        expect(Q.CountOccurrence("Test")).toBe(2);
+    });
     it('returns 0 if no occurrences', () => {
-      Q.enqueue('Test')
-      Q.enqueue('Test(1)')
-      Q.enqueue('Test2')
-      Q.enqueue('Test2(1)')
+        Q.enqueue("Test")
+        Q.enqueue("Test(1)")
+        Q.enqueue("Test2")
+        Q.enqueue("Test2(1)")
+        
+        expect(Q.CountOccurrence("Test3")).toBe(0);
+    });
+   
+});
 
-      expect(Q.CountOccurrence('Test3')).toBe(0)
-    })
-  })
+
 })
